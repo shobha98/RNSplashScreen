0a0c90a51e9378722f705e059c072bf32e5fe192
shobha98 <shrivastava.shobha@daffodilsw.com>
2022-05-18 16:22:34 +0530

    added ios splash

--- App.js
+++ App.js
@@ -4,9 +4,10 @@ import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
   useEffect(() => {
-    setTimeout(() => {
-      SplashScreen.hide();
-    }, 2000);
+    // setTimeout(() => {
+    //   SplashScreen.hide();
+    // }, 2000);
+    SplashScreen.hide();
   }, []);
 
   return (
